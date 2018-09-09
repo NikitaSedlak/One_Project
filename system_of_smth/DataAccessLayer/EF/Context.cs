009d3256636b5bf5e40c018142738b3f4f69cf4c
NikitaSedlak <31969832+NikitaSedlak@users.noreply.github.com>
2018-09-09 22:17:41 +0300

    DAL

---
+++ system_of_smth/DataAccessLayer/EF/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+//using DataAccessLayer.
+
+namespace DataAccessLayer.EF
+{
+    public class Context : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ComplexArchitectureDB;Trusted_Connection=True;");
+        }
+    }
+}
